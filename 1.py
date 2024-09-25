06eb72f4c7880cfb6b5bdf9d11c630f96e9f1386
AndresGallardo-hub <andres.nahuel.gallardo@gmail.com>
2024-09-24 22:19:38 -0300

    Actividad practica 1

---
+++ 1.py
@@ -0,0 +1,18 @@
+ Online Python - IDE, Editor, Compiler, Interpreterprint
+# 1) Crea un código que imprima en pantalla la expresión 'Me encanta estudiar Python '
+#print("me encanta estudiar python")
+
+# 2) Crea un codigo que imprima la expresion 'Estudiar con "Python Total" es super divertido '
+#print("Estudiar con es super divertido")
+
+# 3) Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
+#print(500+55)
+# 4) Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta: ¿Qué estás estudiando? El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+#print("¿Qué estás estudiando?"+ input("¿Qué estás estudiando?: "))
+# 5) Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta: ¿En qué país vives?
+#print("tu pais es" + input("en que pais vives: ")
+# 6) Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones: 
+# Escribe tu nombre:
+# Escribe tu apellido:
+# El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+#print("tu nombre completo es " + input("dime tu nombre: " \n "dime tu apellido: "))
