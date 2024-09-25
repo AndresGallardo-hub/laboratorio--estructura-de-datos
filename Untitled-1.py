7bab574c89983664f01a7d777bcc3d89614cc6c4
AndresGallardo-hub <andres.nahuel.gallardo@gmail.com>
2024-09-24 22:21:55 -0300

    Proyecto Dia 2

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+nombreEmpleado = input("dime tu nombre ")
+ventasdelmes = 0
+ventasdelmes=float(input ( "por favor ingrese las ventas del mes:" ))
+gananciadelmes = (ventasdelmes*13/100)
+print (f"las ventas del mes son {ventasdelmes} su ganancia es $ {round(gananciadelmes, 2)} ")
