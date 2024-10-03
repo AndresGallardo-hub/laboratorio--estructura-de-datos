a01acf3740a5299c7309fd198742875c55d65952
AndresGallardo-hub <andres.nahuel.gallardo@gmail.com>
2024-10-02 21:32:03 -0300

    Actividad Clase 4 Parte 1

---
+++ 2'10.py
@@ -0,0 +1,88 @@
+#comparacion 1
+#num_1 = 36
+#num_2 = 17
+#mi_bool = num_1 >= num_2
+#print(mi_bool)
+
+#comparacion 2
+#num1 = 25 **0.5
+
+#num2 = 5
+
+#mi_bool = num1 == num2
+#print(mi_bool)
+
+
+#comparacion 3
+#num1= 64 *3
+
+#num2 =24 * 8
+
+#mi_bool=num2!=num1
+
+#print(mi_bool)
+
+#operadores logicos 1
+#num1=36
+
+#num2=72/2
+
+#num3=48
+#mi_bool = num1>num2 and num1<num3
+#print(mi_bool)
+
+#operadores logicos 2 
+#num1=36
+
+#num2=72/2
+
+#num3=48
+#mi_bool = num1 > num2 or num1 < num3
+#print(mi_bool)
+
+#operadores logicos 3
+#frase = " Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan"
+
+#palabra1 = "exito"
+#palabra2 = "tecnologia"
+#mi_bool = frase.__contains__ (palabra1 and palabra2) 
+#mi_bool =  palabra1 and palabra2 in frase
+#print(mi_bool)
+
+#control de flujo 1
+#num1 = int(input("Ingresa un número:"))
+
+#num2 = int(input("Ingresa otro número:"))
+
+
+#if num1 > num2 : 
+#   print"{num1} es mayor que {num2}"
+
+#elif num1<num2 :
+#   print("num2 es mayor que num1")
+#elif num1 == num2
+#   print("num1 y num2 son iguales")
+#else("no elijio numero")
+
+#control de flujo 2
+# edad = 16
+# tiene_licencia = False
+# if edad >= 18 :
+#     print("Puedes conducir")
+# elif edad < 18  :
+#     print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
+# else  :
+#     print("No puedes conducir. Necesitas contar con una licencia")
+#control de flujo 3
+# habla_ingles = True
+# sabe_python = False
+# if habla_ingles and sabe_python :
+#     print("Cumples con los requisitos para postularte")
+# elif not sabe_python and not habla_ingles :
+#     print("Para postularte, necesitas saber programar en Python y tener conocimientos de inglés")
+# elif not habla_ingles and sabe_python :
+#     print("Para postularte, necesitas tener conocimientos de inglés")
+# else :
+#     print("Para postularte, necesitas saber programar en Python")
+
+
